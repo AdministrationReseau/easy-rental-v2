fcc12119197eab2a799f7e2544a185b3adf45b74
Ntye Ebo'o Nina Laissa <ntye.nina@gmail.com>
2025-04-15 07:33:31 +0100

    translation changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n: {
-    locales: ['en', 'fr'],
-    defaultLocale: 'en',
-    localeDetection: true,
+    locales: ['default', 'en', 'fr'],
+    defaultLocale: 'default',
+    localeDetection: false,
   },
   // other next.js configurations...
 };
